c0dba6ba1ee79858a47a31e58cbc2b1045a52630
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2019-10-19 17:46:10 -0500

    [FEAT] catalog working

--- src/Card.js
+++ src/Card.js
@@ -3,14 +3,13 @@ import React, { Component } from 'react'
 class Card extends Component {
     render() {
         return(
-            <div className="col-12 col-sm-4">
-            <div className="card">
-                <img src="https://picsum.photos/200" className="card-img-top" alt="..." />
+            <div className="col-12 col-sm-4 d-flex justify-content-between">
+            <div className="card d-flex justify-content-between">
+                <img src={this.props.imgUrl} className="card-img-top" alt="..." />
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of
-                        the card's content.</p>
-                    <p className="text-danger text-right">$5000.00</p>
+                    <h5 className="card-title">{this.props.name}</h5>
+                    <p className="card-text">{this.props.description}</p>
+                    <p className="text-danger text-right">{this.props.price}</p>
                     <a href="#" className="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
